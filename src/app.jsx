57b3086e1304c12e862d481aaed64f6283218c66
Estrio <johnchristian.fortuno.b@gmail.com>
2026-07-17 21:33:10 +0800

    WhyChoose.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -4,6 +4,7 @@ import About from "./sections/About";
 import Services from "./sections/Services"
 import Portfolio from "./sections/Portfolio";
 import Process from "./sections/Process";
+import WhyChoose from "./sections/WhyChoose";
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
             <Services/>
             <Portfolio/>
             <Process/>
+            <WhyChoose />
         </> 
     );
 }
